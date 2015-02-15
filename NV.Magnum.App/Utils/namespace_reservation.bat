613d21eb82a3d7c3d5da73efa91bceb1212ceb91
nvsnkv <nvsnkv@live.com>
2015-02-15 15:42:05 +0300

    changed default port number in reservation script

--- NV.Magnum.App/Utils/namespace_reservation.bat
+++ NV.Magnum.App/Utils/namespace_reservation.bat
@@ -1,7 +1,7 @@
 REM this script reserves urls for embedded magnum webserver
 REM usage:
-REM		add_namespace_reservation add 2049
-REM		add_namespace_reservation delete 2049
+REM		namespace_reservation add 33026
+REM		namespace_reservation delete 33026
 REM http://stackoverflow.com/questions/4019466/httplistener-access-denied/4115328#4115328
 REM port number should be synced with app.config!
 REM run as admin, but from current user!
